2668af4de6f8c18933a377730ee32c741fd29ea5
Sandra Moen <sandramoen01@gmail.com>
2022-02-25 10:23:20 +0100

    improved leaf movement

--- core/src/no/sandramoen/hunted/actors/Leaf.kt
+++ core/src/no/sandramoen/hunted/actors/Leaf.kt
@@ -3,13 +3,12 @@ package no.sandramoen.hunted.actors
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.utils.Align
 import no.sandramoen.hunted.utils.BaseActor
 import no.sandramoen.hunted.utils.BaseGame
 
 class Leaf(x: Float, y: Float, s: Stage, color: Color, layer: Int) : BaseActor(x, y, s) {
-    private val tempo = MathUtils.random(.005f, .01f)
-
     init {
         loadImage("leaf")
 
@@ -24,13 +23,45 @@ class Leaf(x: Float, y: Float, s: Stage, color: Color, layer: Int) : BaseActor(x
         centerAtPosition(x, y)
         setRandomPosition()
         this.color = color
+
+        move()
+        rotate()
     }
 
     override fun act(dt: Float) {
         super.act(dt)
-        y -= tempo
         if (y + height < 0)
             respawn()
+    }
+
+    private fun move() {
+        val amountX = MathUtils.random(.1f, 1.2f)
+        val amountY = MathUtils.random(.75f, 1.5f)
+        val duration = MathUtils.random(8f, 12f)
+        val direction = if (MathUtils.randomBoolean()) -1f else 1f
+        addAction(
+            Actions.forever(
+                Actions.sequence(
+                    Actions.moveBy(direction * amountX, -2 * amountY, duration),
+                    Actions.moveBy(2 * amountX * -direction, -4 * amountY, 2 * duration),
+                    Actions.moveBy(direction * amountX, -2 * amountY, duration)
+                )
+            )
+        )
+    }
+
+    private fun rotate() {
+        val duration = 4f
+        val rotationAmount = MathUtils.random(10f, 20f)
+        addAction(
+            Actions.forever(
+                Actions.sequence(
+                    Actions.rotateBy(rotationAmount, duration),
+                    Actions.rotateBy(-2 * rotationAmount, duration),
+                    Actions.rotateBy(rotationAmount, duration)
+                )
+            )
+        )
     }
 
     private fun setRandomPosition() {
